1cd6723af94b0f515b02e1b20b1339b3800712e2
yagobonardi <yagobonardi@gmail.com>
2020-12-16 12:10:54 -0300

    first commit

---
+++ JsonViewer/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace JsonViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.lbljson = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(40, 513);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(113, 23);
+            this.btnFormat.TabIndex = 0;
+            this.btnFormat.Text = "format";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.BtnFormat_Click);
+            // 
+            // lbljson
+            // 
+            this.lbljson.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lbljson.AutoSize = true;
+            this.lbljson.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbljson.Location = new System.Drawing.Point(6, 16);
+            this.lbljson.Name = "lbljson";
+            this.lbljson.Size = new System.Drawing.Size(30, 15);
+            this.lbljson.TabIndex = 1;
+            this.lbljson.Text = "{ ... }";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(159, 513);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(113, 23);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "copy formated";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(278, 513);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(113, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Click += new System.EventHandler(this.NotifyIcon_Click);
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseClick);
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbljson);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 495);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(439, 548);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnFormat);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "{ Json Viewer }";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.Label lbljson;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ JsonViewer/Form1.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Windows.Forms;
+
+namespace JsonViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Format();
+        }
+
+        private void BtnFormat_Click(object sender, EventArgs e)
+        {
+            Format();
+        }
+
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(lbljson.Text);
+            SetButtonCopyText("copied!");
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ClearPrincipalLabel();
+            SetButtonCopyText("copy");
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            OpenByIcon();
+            Format();
+        }
+
+        private void NotifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            OpenByIcon();
+            Format();
+        }
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            OpenByIcon();
+            Format();
+        }
+
+        private void OpenByIcon()
+        {
+            this.Show();
+
+            this.WindowState = FormWindowState.Normal;
+
+            this.notifyIcon.Visible = false;
+        }
+
+        private void ClearPrincipalLabel()
+        {
+            lbljson.Text = "{ ... }";
+        }
+
+        private void SetButtonCopyText(string text)
+        {
+            btnCopy.Text = text;
+        }
+
+        private void Format()
+        {
+            ClearPrincipalLabel();
+            SetButtonCopyText("copy");
+
+            if (Clipboard.ContainsText() && Clipboard.GetText() != string.Empty)
+            {
+                try
+                {
+                    var text = Clipboard.GetText();
+
+                    var obj = JsonConvert.DeserializeObject(text);
+
+                    var f = JsonConvert.SerializeObject(obj, Formatting.Indented);
+
+                    lbljson.Text = f;
+                }
+                catch (Exception ex)
+                {
+                    ClearPrincipalLabel();
+                    MessageBox.Show(ex.Message, "copy a VALID text to format");
+                }
+
+            }
+            else
+            {
+                ClearPrincipalLabel();
+                MessageBox.Show("copy a text to format!", "error");
+            }
+        }
+    }
+}
